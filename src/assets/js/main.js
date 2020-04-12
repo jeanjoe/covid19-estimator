29f3640d917ef948242dbd05b0b5246f2dd60488
jeanjoe <manzede@gmail.com>
2020-04-12 14:59:50 +0300

    change node version and fix lint errors

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,11 +1,11 @@
-import estimator from '../../estimator.js';
+// import estimator from '../../estimator.js';
 
-const submitEstimate = (event) => {
-  event.preventDefault();
-  console.log('clicked');
-  const data = {
-    region: 'Africa'
-  };
+// const submitEstimate = (event) => {
+//   event.preventDefault();
+//   console.log('clicked');
+//   const data = {
+//     region: 'Africa'
+//   };
 
-  estimator.covid19ImpactEstimator(data);
-};
+//   estimator.covid19ImpactEstimator(data);
+// };
